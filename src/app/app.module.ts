7bc7953eacee225508757452c4dcda5e6cae0045
vinettvictor <v.vinett01@ufromail.cl>
2021-09-24 14:10:29 -0300

    Proyecto principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './compartida/header/header.component';
+import { FooterComponent } from './compartida/footer/footer.component';
+import { AsideComponent } from './compartida/aside/aside.component';
+import { CategoriasComponent } from './vistas/categorias/categorias.component';
+import { ProductosComponent } from './vistas/productos/productos.component';
+import { LoginComponent } from './vistas/login/login.component';
+import { RegistroComponent } from './vistas/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AsideComponent,
+    CategoriasComponent,
+    ProductosComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
