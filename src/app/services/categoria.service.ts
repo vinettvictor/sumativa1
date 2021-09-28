935ced516508ea60f47ef924eb5ecc2e74de3c08
vinettvictor <v.vinett01@ufromail.cl>
2021-09-28 10:42:09 -0300

    creacion inicial de interfaces

--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -10,17 +10,31 @@ export class CategoriaService {
   constructor(private http: HttpClient) {
     console.log("categoria service")
     this.cargarCategorias();
+    //this.cargarCategoriasWeb();
   }
 
   private cargarCategorias(){
     console.log("cargando categorias");
 
     //estructura base para consumir apis
-    //this.http.get('url').subscribe((respuesta) => {});;
-    this.http.get('assets/data/producto.json').subscribe(
-      (respuesta) => {
-        console.log(respuesta);
+    //this.http.get('url').subscribe((respuesta) => {});
+
+    //lectura desde archivo json local
+    this.http.get('assets/data/categoria.json').subscribe(
+      (respuesta: any) => {
+        console.log("Respuesta: ",respuesta);
+        console.log("Productos: ",respuesta['frutas']);
+        console.log("Pera: ",respuesta['frutas'][0]);
 
       });
   }
+
+  private cargarCategoriasWeb(){
+     //lectura desde la web
+      this.http.get('https://learsixela.github.io/productos_json/productos.json').subscribe(
+      (respuesta) => {
+        console.log("Respuesta desde la web: ", respuesta);
+    });
+  }
+
 }
