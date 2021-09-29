b42c7a94d5f36df958e85f24cbe2d5c17890cd20
vinettvictor <v.vinett01@ufromail.cl>
2021-09-28 21:52:24 -0300

    se agregar comentarios en al función agregar

--- src/app/vistas/productos/productos.component.ts
+++ src/app/vistas/productos/productos.component.ts
@@ -20,14 +20,15 @@ export class ProductosComponent implements OnInit {
 
 
   agregar(id: number | any){
-    id = id-1;
-    this.listaProductos.push(this.productosService.productos[id]);
+    id = id-1; //inicializamos la id en 0
+    this.listaProductos.push(this.productosService.productos[id]); // agregamos los productos a nuestra nueva lista de productos
     console.log("Arreglos: ",this.listaProductos);
 
+    //recorremos la lista para comparar si las id son iguales
     for(let prod in this.productosService.productos){
       if(this.productosService.productos[id].id == this.productosService.productos[prod].id){  
-        this.listaProductos[prod].cantidad = this.listaProductos[prod].cantidad-1;
-        this.listaProductos.join(this.listaProductos[prod]);     
+        this.listaProductos[prod].cantidad = this.listaProductos[prod].cantidad-1; // si las id coinciden , descuenta 1 de cantidad.
+        this.listaProductos.join(this.listaProductos[prod]); //agregamos los datos actualizados al arreglo, pero solo reemplazando el valor "cantidad"
       } else {
         console.log("los id no son igual ")
       }
