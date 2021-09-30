6f404c2a9f4641e42290516b4078f24626db1e99
vinettvictor <v.vinett01@ufromail.cl>
2021-09-30 11:33:34 -0300

    se agrga validación de formulario de envio

--- src/app/vistas/pago/pago.component.ts
+++ src/app/vistas/pago/pago.component.ts
@@ -6,10 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pago.component.css']
 })
 export class PagoComponent implements OnInit {
+  
+  pais: string = "";
+  calle: string = "";
+  ciudad: string = "";
+  region: string = "";
+  codPostal: string = "";
+
+  errPais: string = "";
+  errCalle: string = "";
+  errCiudad: string = "";
+  errRegion: string = "";
+  errCodPostal: string = "";
+
+  validaEnvio: boolean = false;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  agregarDireccion(): void{
+    console.log("Estamos en agregar");
+    this.errPais = "";
+    this.errCalle = "";
+    this.errCiudad = ""; 
+    this.errRegion ="";
+    this.errCodPostal ="";
+    this.validaEnvio = false;
+
+    if(this.pais==""){
+      this.errPais = "Por favor ingrese un valor al campo: Pais";
+    } 
+    if (this.calle==""){
+      this.errCalle = "Por favor ingrese un valor al campo: Calle";
+    }
+    if (this.ciudad==""){
+      this.errCiudad = "Por favor ingrese un valor al campo: Ciudad";
+    }
+    if(this.region==""){
+      this.errRegion = "Por favor ingrese un valor al campo: Región";
+    }
+    if(this.codPostal==""){
+      this.errCodPostal = "Por favor ingrese un valor al campo: Código Postal";
+    }
+
+    if(this.pais!="" && this.calle!="" && this.ciudad!="" && this.region!="" && this.codPostal!=""){
+      this.validaEnvio = true;
+    }
+  }
+
+  agregarTarjeta(){
+
+  }
 }
